30c025896ee9659d1acf1992bca4b531b9924b65
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-05-29 08:55:39 +0200

    Verwendung von File::Find überarbeiten und die gefundenen Werte weiter verarbeiten.
    Die von File::Find gefundenen Werte nicht zurückgeben und weiter-
    verarbeiten lassen sondern direkt mit anonymen Funktionen im Auf-
    ruf von File::Find die entsprechenden Befehle ausführen. Damit ist
    das Problem, dass ein Array aus zurückgegebenen Werten immer leer
    ist behoben. Die Werte werden direkt beim 'find' mithilfe von 'push'
    in ein Array geschoben und danach mit den Funktionen csv und
    open_file_to_write zu csv-Daten gemacht und in eine csv-Datei ge-
    schrieben.

--- csvAusPfad_fileFindtest.pl
+++ csvAusPfad_fileFindtest.pl
@@ -2,7 +2,7 @@
 
 # Autor: Josef Florian Sedlmeier
 # Datum: 2013/05/28
-# CSV-Datei aus Array erstellen
+# CSV-Datei aus Array erstellen (using File::Find)
 
 use strict;
 use warnings;
@@ -10,30 +10,37 @@ use warnings;
 use Log::Log4perl qw( :easy );
 use File::Find;
 
-# Initialisieren von Log4perl
+# Initialisieren von Log4perl.
 Log::Log4perl->easy_init( $DEBUG );
 
-my $dir = "/home/josef/Bilder";
+my $dir = "/home/extcjs/Bilder";
 my @dateien;
-push(@dateien, find(\&wanted, $dir) );
-find(\&wanted, $dir);
-#DEBUG @dateien;
-#&open_file_to_write(@dateien);
 
-sub wanted {
-    
-    print if -f;
-}
+# mit File::Find und einer anonymen Funktion als Parameter den Ordner durchsuchen
+# und das Ergebnis in das Array @dateien schreiben, wenn es nicht mit '.' oder 
+# '..' beginnt. 
+find(sub { if ($_ !~ m/^\..*/) {
+                push @dateien, $_
+                }
+         }, $dir
+    );
+# Den Inhalt von @dateien aufsteigend sortieren und mit der Funktion 'csv' weiter
+# bearbeiten.
+my @gesuchteDateien = &csv(sort(@dateien));
+# Das (nun mit Kommas getrennte) Array anschließend in die Datei 'output.csv'
+# schreiben. 
+&open_file_to_write(@gesuchteDateien);
 
-# Subroutine um eine csv-Liste zu erstellen
+# Subroutine um eine csv-Liste zu erstellen.
 sub csv {
     my @csvEintraege = join (",", @_);
     return @csvEintraege;
 }
-
+# Subroutine um die Datei 'output.csv' zu öffnen und das übergebene Array rein
+# zu schreiben.
 sub open_file_to_write {
     my $csv = "output.csv";
-    open (DATEI, "$csv") or die $!;
+    open (DATEI, ">$csv") or die $!;
     print DATEI ( @_ );
     close (DATEI);
 }
