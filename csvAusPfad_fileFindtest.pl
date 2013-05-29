49ba8b71efda2c1beae287e767563a61eb75c318
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-05-29 11:08:23 +0200

    Suchpfad über Argumente angeben anstatt fest zu hinterlegen

--- csvAusPfad_fileFindtest.pl
+++ csvAusPfad_fileFindtest.pl
@@ -12,32 +12,57 @@ use File::Find;
 
 # Initialisieren von Log4perl.
 Log::Log4perl->easy_init( $DEBUG );
+# Argumente übernehmen und in Variablen zwischenspeichern.
+my $was = $ARGV[0];
+my $nr = $ARGV[1];
+my $unterverz = $ARGV[2];
+my $prefix = "/home/josef/";
+my $dir;
+my $num_args = $#ARGV + 1;
+# prüfen ob, und wieviele Argumente angegeben wurden und entsprechend den daraus
+# erstellten Pfad anpassen.
+if ($num_args == 1) {
+    $dir = $prefix . $was . "/";
+    &main($dir);
+} elsif ($num_args == 2) {
+    $dir = $prefix . $was . "/" . $nr . "/";
+    &main($dir);
+} elsif ($num_args == 3) {
+    $dir = $prefix . $was . "/" . $nr . "/" . $unterverz . "/";
+    &main($dir);
+} else {
+    print "Zu wenig oder zu viele Argumente!\n";
+    exit;
+}
 
-my $dir = "/home/extcjs/Bilder";
-my @dateien;
-# mit File::Find und einer anonymen Funktion als Parameter den Ordner durchsuchen
-# und das Ergebnis in das Array @dateien schreiben, wenn es nicht mit '.' oder 
-# '..' beginnt. 
-# Der Reguläre Ausdruck '/^.*(?:jpg|gif|png)$/' prüft, ob die Dateiendung jpg,
-# gif oder png ist damit nur diese Dateien zurückgegeben werden.
-find(sub { if ( ($_ !~ m/^\..*/) and ( $_ =~ /^.*(?:jpg|gif|png)$/ ) and (-f $_) ){
-                push @dateien, $_
-                }
-         }, $dir
-    );
-print "Verzeichnis gelesen!\n";
-# Den Inhalt von @dateien aufsteigend sortieren und mit der Funktion 'csv' weiter
-# bearbeiten.
-my @gesuchteDateien = &csv(sort(@dateien));
-# Das (nun mit Kommas getrennte) Array anschließend in die Datei 'output.csv'
-# schreiben. 
-&open_file_to_write(@gesuchteDateien);
-print "Datei './output.csv' erstellt!\n";
+DEBUG $dir;
+
+sub main {
+    my @dateien;
+    # mit File::Find und einer anonymen Funktion als Parameter den Ordner durchsuchen
+    # und das Ergebnis in das Array @dateien schreiben, wenn es nicht mit '.' oder 
+    # '..' beginnt. 
+    # Der Reguläre Ausdruck '/^.*(?:jpg|gif|png)$/' prüft, ob die Dateiendung jpg,
+    # gif oder png ist damit nur diese Dateien zurückgegeben werden.
+    find(sub { if ( ($_ !~ m/^\..*/) and ( $_ =~ /^.*(?:jpg|gif|png)$/ ) and (-f $_) ){
+                    push @dateien, $_
+                    }
+             }, $dir
+        );
+    # Den Inhalt von @dateien aufsteigend sortieren und mit der Funktion 'csv' weiter
+    # bearbeiten.
+    my @gesuchteDateien = &csv(sort(@dateien));
+    # Das (nun mit Kommas getrennte) Array anschließend in die Datei 'output.csv'
+    # schreiben. 
+    &open_file_to_write(@gesuchteDateien);
+}
+
 # Subroutine um eine csv-Liste zu erstellen.
 sub csv {
     my @csvEintraege = join (",", @_);
     return @csvEintraege;
 }
+
 # Subroutine um die Datei 'output.csv' zu öffnen und das übergebene Array rein
 # zu schreiben.
 sub open_file_to_write {
@@ -45,4 +70,5 @@ sub open_file_to_write {
     open (DATEI, ">$csv") or die $!;
     print DATEI ( @_ );
     close (DATEI);
+    print "Datei './output.csv' erstellt!\n";
 }
