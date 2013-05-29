970dacbe892b40ea64170e16577d422a23b3e1bf
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-05-29 10:12:01 +0200

    Dateiendung überprüfen ob jpg, gif oder png
    Damit nur Dateien im Format jpg, gif oder png in die Liste ge-
    schrieben werden, wurde das if-Statement in der Anonymen Funktion,
    die als Parameter beim 'find'-Aufruf dient um einen Regulären
    Ausdruck erweitert.

--- csvAusPfad_fileFindtest.pl
+++ csvAusPfad_fileFindtest.pl
@@ -15,22 +15,24 @@ Log::Log4perl->easy_init( $DEBUG );
 
 my $dir = "/home/extcjs/Bilder";
 my @dateien;
-
 # mit File::Find und einer anonymen Funktion als Parameter den Ordner durchsuchen
 # und das Ergebnis in das Array @dateien schreiben, wenn es nicht mit '.' oder 
 # '..' beginnt. 
-find(sub { if ($_ !~ m/^\..*/) {
+# Der Reguläre Ausdruck '/^.*(?:jpg|gif|png)$/' prüft, ob die Dateiendung jpg,
+# gif oder png ist damit nur diese Dateien zurückgegeben werden.
+find(sub { if ( ($_ !~ m/^\..*/) and ( $_ =~ /^.*(?:jpg|gif|png)$/ ) and (-f $_) ){
                 push @dateien, $_
                 }
          }, $dir
     );
+print "Verzeichnis gelesen!\n";
 # Den Inhalt von @dateien aufsteigend sortieren und mit der Funktion 'csv' weiter
 # bearbeiten.
 my @gesuchteDateien = &csv(sort(@dateien));
 # Das (nun mit Kommas getrennte) Array anschließend in die Datei 'output.csv'
 # schreiben. 
 &open_file_to_write(@gesuchteDateien);
-
+print "Datei './output.csv' erstellt!\n";
 # Subroutine um eine csv-Liste zu erstellen.
 sub csv {
     my @csvEintraege = join (",", @_);
