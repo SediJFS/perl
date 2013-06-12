f1c5b0e127e1f94991aed6a8ff664847dd5e26dd
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-12 14:06:49 +0200

    Anpassen des MailChecker Skript um e mit Conky nutzen zu können
    Damit man das Skript nicht nur im Terminal ausführen kann, wurde es
    so angepasst, dass die Zugangsdaten nichtmehr abgefragt werden,
    sondern direkt in der Datei stehen. Nun lässt sich das Skript in
    Conky integrieren. Zusätzlich wurde MailChecker.pl etwas entschlackt
    und aufgeräumt.

--- MailChecker/MailChecker4conky.pl
+++ MailChecker/MailChecker4conky.pl
@@ -2,24 +2,26 @@
 
 # Autor: Josef Florian Sedlmeier
 # Datum: 2013/04/07
-# IMAP Mail Checker mit SSL
+# IMAP Mail Checker mit SSL für conky
 
 use strict;
 use warnings;
 
+# use Log::Log4perl qw( :easy );
+# Log::Log4perl->easy_init( $DEBUG );
 # Bibliothek für IMAP mit SSL laden.
 # Das Paket libnet-imap-simple-ssl-perl muss installiert sein!
 use Net::IMAP::Simple;
 use Net::IMAP::Simple::SSL;
 use Term::ReadKey;
 
-my $server = 'imap.gmail.com';
-print( "Bitte geben Sie Ihre Googlemail-Adresse ein: \n" );
-my $user = &getpassphrase;
-chomp( $user );
-print( "\nBitte geben Sie ihr Googlemail Passwort ein: \n" );
-my $pass = <STDIN>;
-chomp( $pass );
+#Zugangsdaten hier eintragen
+# Serveradresse
+my $server = '<yourSSL-IMAPServer>';
+# Emailadresse
+my $user = '<yourEmail>';
+# Passwort
+my $pass = '<yourPassword>';
 
 # Neue Instanz des SSL-Objektes anlegen mit $server als übergebenem Parameter.
 my $imap = Net::IMAP::Simple::SSL->new( $server );
@@ -27,27 +29,8 @@ my $imap = Net::IMAP::Simple::SSL->new( $server );
 $imap->login( $user => $pass );
 # Mails aus Posteingang abrufen und ungelesene zählen.
 my $messages = $imap->search_unseen( 'INBOX' );
-
 # Verbindung zum IMAP-Konto wieder trennen.
 $imap->quit();
 
 # Ausgabe 
 print( "\nUngelesen: ", $messages, "\n" );
-
-# Subroutine um Passworteingabe mit Sternchen zu überschreiben.
-sub getpassphrase {
-	print "enter passphrase: ";
-	ReadMode 'raw';
-	my $passphrase;
-	while (1) {
-		my $key .= (ReadKey 0);
-		if ($key ne "\n") {
-			print '*';
-			$passphrase .= $key
-		} else {
-			last
-		}
-	}
-	ReadMode 'restore';
-	return $passphrase
-}	
