482c8956dcdae24e423d2625b0a4fd648af9ee9a
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-17 13:45:36 +0200

    Kommentare einfügen
    Zusätzliche Kommentare in Code einfügen um das genaue Vorgehen und
    die Objektorrientierte Verwendung von Modulen genauer zu erklären.

--- OOP/006_Module.pl
+++ OOP/006_Module.pl
@@ -3,7 +3,10 @@
 use strict;
 use warnings;
 
+# Verwendung von Math::BigInt rein Objektorientiert.
 use Math::BigInt;
+# Aufruf mit 'new' zum Anlegen einer neuen Instanz.
 my $value = Math::BigInt->new( 2 ); # Mit 2 beginnen
+# Methodenaufruf über die angelegte Instanz.
 $value->bpow(1000);                 # 2 ^ 1000 (2 hoch 1000) berechnen
 print $value->bstr(), "\n";         # Ergebnis ausgeben
