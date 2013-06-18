4c0165fb093f7fc41f33c7a9e323588bbe91bd9f
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-18 13:12:34 +0200

    Hinzufügen mehrdimensionaler Arrays (Verschachtelte Referenzen)

--- OOP/003_Referenzen.pl
+++ OOP/003_Referenzen.pl
@@ -30,10 +30,26 @@ sub check_required_items {
 }
 # Arrays definieren
 my @skipper = qw(blue_shirt hat jacket preserver sunscreen);
+my @skipper_with_name = ("Skipper", \@skipper);
 my @professor = qw(sunscreen water_bottle slide_rule batteries radio);
+my @professor_with_name = ("Professor", \@professor);
 my @gilligan = qw(red_shirt hat lucky_socks water_bottle);
+my @gilligan_with_name = ("Gilligan", \@gilligan);
+
+my @all_with_names = (
+    \@skipper_with_name,
+    \@professor_with_name,
+    \@gilligan_with_name,
+);
+
 # Subroutine aufrufen
 # Dabei das Array jeweils über eine Referenz ansprechen
-check_required_items("professor", \@professor);
-check_required_items("skipper", \@skipper);
-check_required_items("gilligan", \@gilligan);
+for my $person (@all_with_names) {
+    my $who = $$person[0];
+    my $provisions_reference = $$person[1];
+    check_required_items($who, $provisions_reference);
+}
+# alter Aufruf vor mehrdimensionaler Arrayreferenz
+#~ check_required_items("professor", \@professor);
+#~ check_required_items("skipper", \@skipper);
+#~ check_required_items("gilligan", \@gilligan);
