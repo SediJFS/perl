a8c601153a797a27652ae4a06b229c6d6a29acd9
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-18 12:04:42 +0200

    Die Arrays nun über Referenzen ansprechen, damit sie nicht bei jeder Verwendung komplett kopiert werden

--- OOP/003_Referenzen.pl
+++ OOP/003_Referenzen.pl
@@ -14,6 +14,7 @@ use warnings;
 # um nicht für jedes Crew-Mitglied den selben Code schreiben zu müssen:
 sub check_required_items {
     my $who = shift;
+    my $items = shift;
     my @required = qw(preserver sunscreen water_bottle jacket);
     for my $item (@required) {
         unless (grep $item eq $_, @_) { # nicht in der Liste gefunden?
@@ -26,6 +27,7 @@ my @skipper = qw(blue_shirt hat jacket preserver sunscreen);
 my @professor = qw(sunscreen water_bottle slide_rule batteries radio);
 my @gilligan = qw(red_shirt hat lucky_socks water_bottle);
 # Subroutine aufrufen
-check_required_items("professor", @professor);
-check_required_items("skipper", @skipper);
-check_required_items("gilligan", @gilligan);
+# Dabei das Array jeweils über eine Referenz ansprechen
+check_required_items("professor", \@professor);
+check_required_items("skipper", \@skipper);
+check_required_items("gilligan", \@gilligan);
