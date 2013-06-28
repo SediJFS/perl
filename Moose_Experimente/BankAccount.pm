87d8ce4a66c52801b939201bbef52ede5745d41b
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-28 12:02:17 +0200

    Methoden-Modifier und Subklassen
    Beispiel für die Verwendung von Methoden-Modifiern und Subklassen mit
    Mosse. In diesem Beispiel liegt das Hauptaugenmerk auf dem Methoden-
    Modifier 'before' und auf der Subklasse 'CheckingAccount'

---
+++ Moose_Experimente/BankAccount.pm
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+#Basisklasse 'BankAccount' erstellen
+package BankAccount;
+use Moose;
+# Attribute der Klasse festlegen
+has 'balance' => ( isa => 'Int', is => 'rw', default => 0 );
+# Klassenmethoden festlegen
+sub deposit {
+    my ( $self, $amount ) = @_;
+    $self->balance( $self->balance + $amount );
+}
+
+sub withdraw {
+    my ( $self, $amount ) = @_;
+    my $current_balance = $self->balance();
+    ( $current_balance >= $amount )
+        || confess "Account overdrawn";
+    $self->balance( $current_balance - $amount );
+}
+
+sub current_balance {
+    my $self = shift;
+    $self->balance;
+}
+
+package CheckingAccount;
+use Moose;
+# Basisklasse erweitern
+extends 'BankAccount';
+# zusätzliche Attribute festlegen
+has 'overdraft_account' => ( isa => 'BankAccount', is => 'rw' );
+# der 'before'-modifier wird VOR der eigentlichen Subroutine ausgeführt. Er wird
+# also praktisch am Anfang der Subroutine eingefügt.
+# Der folgende Codeblock überprüft, ob das Konto eine genügende Deckung hat, um
+# eine Abbuchung vorzunehmen.
+before 'withdraw' => sub {
+    my ( $self, $amount ) = @_;
+    my $overdraft_amount = $amount - $self->balance();
+    if ( $self->overdraft_account && $overdraft_amount > 0 ) {
+        $self->overdraft_account->withdraw( $overdraft_amount );
+        $self->deposit( $overdraft_amount );
+    }
+};
+
+# Objekte anlegen und was damit machen.
+my $konto = BankAccount->new();
+$konto->deposit(5000);
+print $konto->current_balance, "\n";
+$konto->withdraw(3000);
+print $konto->current_balance, "\n";
+$konto->withdraw(2500);
+print $konto->current_balance, "\n";
