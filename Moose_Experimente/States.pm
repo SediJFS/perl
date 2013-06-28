5436a1c2c4f1f80344e58f5e24ab858e16ba5614
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-28 16:11:13 +0200

    Ausgabe von Land als Name anstatt als Hashwert

--- Moose_Experimente/States.pm
+++ Moose_Experimente/States.pm
@@ -7,33 +7,33 @@ use Locale::Country;
 has 'country' => ( isa => 'Str', is => 'rw');
 has 'code' => ( isa => 'Str', is => 'rw' );
 
-#~ sub get_country_code {
-    #~ my $self = shift;
-    #~ my $country = code2country($self);
-    #~ if (defined $country)
-    #~ {
-        #~ return "$self = $country\n";
-    #~ }
-    #~ else
-    #~ {
-        #~ return "'$self' is not a valid country code!\n";
-    #~ }
-#~ }
-#~ my $searchedCountry = Country->new();
-#~ print "Enter country code: ";
-    #~ chop(my $code = <STDIN>);
-#~ $searchedCountry->code($code);
-#~ my @ausgabe = $searchedCountry->get_country_code;
-#~ print @ausgabe;
-
-print "Enter country code: ";
-    chop(my $code = <STDIN>);
-    my $country = code2country($code);
+sub get_country_code {
+    my $self = shift;
+    my $country = code2country($self->code);
     if (defined $country)
     {
-        print "$code = $country\n";
+        return $self->code . "= $country\n";
     }
     else
     {
-        print "'$code' is not a valid country code!\n";
+        return $self->code ." is not a valid country code!\n";
     }
+}
+my $searchedCountry = Country->new();
+print "Enter country code: ";
+    chop(my $code = <STDIN>);
+$searchedCountry->code($code);
+my @ausgabe = $searchedCountry->get_country_code;
+print @ausgabe;
+
+#~ print "Enter country code: ";
+    #~ chop(my $code = <STDIN>);
+    #~ my $country = code2country($code);
+    #~ if (defined $country)
+    #~ {
+        #~ print "$code = $country\n";
+    #~ }
+    #~ else
+    #~ {
+        #~ print "'$code' is not a valid country code!\n";
+    #~ }
