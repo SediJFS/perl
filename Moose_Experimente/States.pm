2df7062c670324f153aa98a50e8ebfa4ad391ca6
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-28 16:50:02 +0200

    Abfrage nach Laenderkennung oder Namen implementieren
    Damit der Benutzer nicht erst in einem Menü entscheiden muss, ob er
    den Ländercode in den Namen oder umgekehrt umwandeln will, wird auf
    Grund der Länge des eingegebenen Strings ermittelt, ob es sich um
    einen Code oder einen Namen handelt. Anschließend wird entsprechend
    dem Ergebnis dieser Abfrage umgewandelt.

--- Moose_Experimente/States.pm
+++ Moose_Experimente/States.pm
@@ -3,37 +3,52 @@
 package Country;
 use Moose;
 use Locale::Country;
+use Log::Log4perl qw( :easy );
+
+# Initialisieren von Log4perl
+Log::Log4perl->easy_init( $DEBUG );
 
 has 'country' => ( isa => 'Str', is => 'rw');
 has 'code' => ( isa => 'Str', is => 'rw' );
 
-sub get_country_code {
+sub get_country_name {
     my $self = shift;
     my $country = code2country($self->code);
+    DEBUG $country;
     if (defined $country)
     {
-        return $self->code . "= $country\n";
+        return $self->code . " = " . $country . "\n";
     }
     else
     {
-        return $self->code ." is not a valid country code!\n";
+        return $self->country ." is not a valid country code!\n";
     }
 }
-my $searchedCountry = Country->new();
-print "Enter country code: ";
-    chop(my $code = <STDIN>);
-$searchedCountry->code($code);
-my @ausgabe = $searchedCountry->get_country_code;
-print @ausgabe;
 
-#~ print "Enter country code: ";
-    #~ chop(my $code = <STDIN>);
-    #~ my $country = code2country($code);
-    #~ if (defined $country)
-    #~ {
-        #~ print "$code = $country\n";
-    #~ }
-    #~ else
-    #~ {
-        #~ print "'$code' is not a valid country code!\n";
-    #~ }
+sub get_country_code {
+    my $self = shift;
+    my $code = country2code($self->country);
+    if (defined $code)
+    {
+        return $self->country . " = " . $code . "\n";
+    }
+    else
+    {
+        return $self->code . " is not a valid country name!\n";
+    }
+}
+
+my $searchedCountry = Country->new();
+print "Enter country code or name: ";
+    chop(my $eingabe = <STDIN>);
+
+my $ergebnis;
+
+if ( $eingabe =~ /^[A-Za-z]{0,2}$/ ) {
+    $searchedCountry->code($eingabe);
+    $ergebnis = $searchedCountry->get_country_name;
+} else {
+    $searchedCountry->country($eingabe);
+    $ergebnis = $searchedCountry->get_country_code;
+}
+print $ergebnis;
