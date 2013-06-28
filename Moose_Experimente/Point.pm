8e5749c44bc555dd4908e41627a2dbc6fbf3e2df
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-28 11:15:30 +0200

    Beispielklasse mit Moose erstellen
    Um die Funktionsweise (und die Einfachheit) von Moose zu testen,
    erstellen wir einen simplen Konstruktor für eine 'Point'-Klasse.
    Diese Klasse definiet die Position eines Punktes in einem Koordinaten-
    System (x und y). Anschließend wird die Klasse erweitert und bekommt
    noch einen dritten Punkt (z).

--- Moose_Experimente/Point.pm
+++ Moose_Experimente/Point.pm
@@ -1,8 +1,9 @@
 #!/usr/bin/env perl
 
+# Konstruktor mit Moose
 package Point;
     use Moose;
-
+# Attribute der Klasse definieren
     has 'x' => (isa => 'Int', is => 'rw', required => 1);
     has 'y' => (isa => 'Int', is => 'rw', required => 1);
 
@@ -12,9 +13,10 @@ package Point;
         $self->y(0);
     }
 
+# Klasse erweitern um ein Attribut
 package Point3D;
     use Moose;
-
+# 'extends' ist vergleichbar mit 'use base'
     extends 'Point';
 
     has 'z' => (isa => 'Int', is => 'rw', required => 1);
@@ -26,14 +28,13 @@ package Point3D;
 
 package main;
 
-    # hash or hashrefs are ok for the constructor
+    # Neue Objekte über den Konstruktor erstellen
     my $point1 = Point->new(x => 5, y => 7);
     my $point2 = Point->new({x => 5, y => 7});
 
     my $point3d = Point3D->new(x => 5, y => 42, z => -5);
 
-    #Ausgabe der frisch erstellten Punkte im Koordinatensystem
-
+    #Ausgabe der frisch erstellten Objekete im Koordinatensystem
     print( "Koordinaten von Punkt1: ",
             $point1->x,
             "|",
