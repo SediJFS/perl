8b50b296214b6c7beaf29d56bf506b143bfe3fd9
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-28 14:08:53 +0200

    Simples Programm zum Umwandeln von Länderkennungen in Namen und umge- kehrt
    Mit dem Modul Locale::Country wird der eingegebene Ländercode (2-stellig)
    in den Ländernamen, oder der eingegebene Ländername in den 2-stelligen
    Ländercode umgewandelt.

--- Moose_Experimente/States.pm
+++ Moose_Experimente/States.pm
@@ -1,6 +1,17 @@
 #!/usr/bin/env perl
 
-package States;
+package Country;
 use Moose;
-use Locale::US;
+use Locale::Country;
 
+print "Enter country code: ";
+    chop(my $code = <STDIN>);
+    my $country = code2country($code);
+    if (defined $country)
+    {
+        print "$code = $country\n";
+    }
+    else
+    {
+        print "'$code' is not a valid country code!\n";
+    }
