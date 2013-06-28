7f3d37cc89c3f5a4ae2c8e1b3b7f1f529b3aed05
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-06-28 14:27:54 +0200

    Erster versuch Locale::Country Objektorientiert zu nutzen... Fehlgeschlagen

--- Moose_Experimente/States.pm
+++ Moose_Experimente/States.pm
@@ -4,6 +4,28 @@ package Country;
 use Moose;
 use Locale::Country;
 
+has 'country' => ( isa => 'Str', is => 'rw');
+has 'code' => ( isa => 'Str', is => 'rw' );
+
+#~ sub get_country_code {
+    #~ my $self = shift;
+    #~ my $country = code2country($self);
+    #~ if (defined $country)
+    #~ {
+        #~ return "$self = $country\n";
+    #~ }
+    #~ else
+    #~ {
+        #~ return "'$self' is not a valid country code!\n";
+    #~ }
+#~ }
+#~ my $searchedCountry = Country->new();
+#~ print "Enter country code: ";
+    #~ chop(my $code = <STDIN>);
+#~ $searchedCountry->code($code);
+#~ my @ausgabe = $searchedCountry->get_country_code;
+#~ print @ausgabe;
+
 print "Enter country code: ";
     chop(my $code = <STDIN>);
     my $country = code2country($code);
