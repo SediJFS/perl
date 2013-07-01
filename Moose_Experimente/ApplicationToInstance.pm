7ac44747f537939762c2486a1b6022c8e719128a
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-07-01 09:51:48 +0200

    Beispielcode vervollständigen

--- Moose_Experimente/ApplicationToInstance.pm
+++ Moose_Experimente/ApplicationToInstance.pm
@@ -1,3 +1,5 @@
+#!/usr/bin/env perl
+
 package MyApp::Role::Job::Manager;
 
 use List::Util qw( first );
@@ -9,3 +11,25 @@ has 'employees' => (
     isa => 'ArrayRef[Employee]',
 );
 
+sub assign_work {
+    my $self = shift;
+    my $work = shift;
+
+    my $employee = first { !$_->has_work } @{ $self->employees };
+
+    die 'all my employees have work to do!' unless $employee;
+
+    $employee->work($work);
+}
+
+package main;
+
+my $lisa = Employee->new( name => 'Lisa' );
+MyApp::Role::Job::Manager->meta->apply($lisa);
+
+my $bart = Employee->new( name => 'Bart' );
+my $homer = Employee->new( name => 'Homer' );
+my $marge = Employee->new( name => 'Marge' );
+
+$lisa->employees( [ $homer, $bart, $marge ] );
+$lisa->assign_work('mow the lawn');
