31ddc53a147ee0331eb19a2ecc14a95f76283e89
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-07-01 12:56:15 +0200

    ShowFile_Moose implementieren
    ShowFile_Moose ist ein simples Skript zum Auslesen einer Datei.
    Das Skipt ist OO erstellt und gibt per Filehandle den Inhalt der
    angegebenen Datei Zeile für Zeile aus.

---
+++ ShowFile_Moose.pm
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+package My::IO;
+
+use Moose;
+use MooseX::GlobRef;
+
+use Log::Log4perl qw( :easy );
+Log::Log4perl->easy_init( $DEBUG );
+
+has 'file' => ( 
+    is          => 'ro',
+    isa         => 'Str',
+    required    => 1
+);
+
+sub open {
+    my $fh = shift;
+    open $fh, $fh->file or confess "cannot open";
+    return $fh;
+}
+
+sub getlines {
+    my $fh = shift;
+    return readline $fh;
+}
+
+print "Bitte geben Sie den Dateipfad an: ";
+my $path = <STDIN>;
+chomp( $path );
+DEBUG $path;
+my $io = My::IO->new( file => $path );
+print "::::::::::::::\n";
+print $io->file, "\n";
+print "::::::::::::::\n";
+$io->open;
+print $io->getlines;
