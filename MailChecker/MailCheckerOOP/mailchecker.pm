567fc011b4fdddcc948fde17bc38a8363c18a990
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-07-16 10:39:08 +0200

    Erweitern des mailchecker-tests

--- MailChecker/MailCheckerOOP/mailchecker.pm
+++ MailChecker/MailCheckerOOP/mailchecker.pm
@@ -13,6 +13,9 @@ use warnings;
 use Log::Log4perl qw( :easy );
 Log::Log4perl->easy_init( $DEBUG );
 
+# Zugriff auf IMAP über SSL
+use Net::IMAP::Simple::SSL;
+
 # OOP-Zugriff auf Dateien:
 use IO::File;
 
@@ -29,8 +32,23 @@ sub new {                                                           #
     my @params = @_;
     bless($self, $class);
     return $self;
+    # Atribute
+    $self->{SERVER} = 'imap.googlemail.com';
+    $self->{USER}   = 'diskilla@gmail.com';
 }
 
 sub get_mail {
     
 }
+
+sub open_imap {
+    my $self   = shift;
+    my @params = @_;
+    my $server = $_[0];
+    my $user   = $_[1];
+    my $imap   = Net::IMAP::Simple::SSL->new( $server );
+    return $imap;
+}
+
+# make perl happy
+1;
--- MailChecker/MailCheckerOOP/t/mailchecker.t
+++ MailChecker/MailCheckerOOP/t/mailchecker.t
@@ -2,9 +2,14 @@
 
 use strict;
 
-use Test::More tests => 1;
+use Test::More tests => 2;
 use lib '..';
 use mailchecker;
 
-my $mail = mailchecker->new();
+use_ok( 'mailchecker' ) or exit;
+
+my $server = 'imap.googlemail.com';
+my $mail = mailchecker->new( $server, 'diskilla@gmail.com');
 isa_ok( $mail, 'mailchecker');
+
+#~ ok( exists $mail->{SERVER} does exist');
