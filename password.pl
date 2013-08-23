42af7ecf7e699ff4bf9d60f57c2589a3a82a4f19
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-08-23 12:15:28 +0200

    Datei mit parameter aufrufen um Passwortlaenge zu bestimmen
    Damit das Skript nicht auf eine bestimmte Passwortlaenge beschraenkt ist
    wird ab sofort ein Parameter uebergeben, der die Laenge bestimmt.

--- password.pl
+++ password.pl
@@ -10,20 +10,32 @@ use warnings;
 use Log::Log4perl qw( :easy );
 
 # Initialisieren von Log4perl
-Log::Log4perl->easy_init( $ERROR );
+Log::Log4perl->easy_init( $DEBUG );
+
+my $num_args = @ARGV;
+
+my (
+    $laenge,
+) = @ARGV
+;
 
 # Zeichen festlegen, die im Passwort vorkommen dürfen
 my @chars = ( "A".."Z", "a".."z", 0..9, qw( ! @ $ % & ) );
-# mithilfe von join und der rand-function ein 8-stelliges Passwort aus dem
-# vorgegebenen Zeichenpool generieren und ausgeben.
-my $password = join("", @chars[ map { rand @chars } ( 1..8 ) ] );
-print($password, "\n");
 
+if (defined($laenge)) {
+    # mithilfe von join und der rand-function ein n-stelliges Passwort aus dem
+    # vorgegebenen Zeichenpool generieren und ausgeben.
+    # wobei n = $laenge
+    my $password = join("", @chars[ map { rand @chars } ( 1..$laenge ) ] );
+    print($password, "\n");
+} else {
+    print "Keine Passwortlänge angegeben! ( 8 bis 10 )\n";
+}
 =pod
 
 =head1 password
 
-password gibt ein zufaelliges, 8 stelliges Passwort aus dem Zeichenpool
+password gibt ein zufaelliges, n-stelliges Passwort aus dem Zeichenpool
 
 "A".."Z"
 "a".."z"
@@ -31,5 +43,13 @@ password gibt ein zufaelliges, 8 stelliges Passwort aus dem Zeichenpool
 ! @ $ % &
 
 zurueck.
+
+=head1 usage
+
+password [laenge]
+
+Beispiel:
+password 15
+gibt ein 15-stelliges Passwort zurück
 
 =cut
